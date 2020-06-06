16d6cde5ad9b1360f726a2ebeafbd680c063e55e
MichielTombergen <michielhttombergen@gmail.com>
2020-06-07 00:57:40 +0200

    Created basic menu like behaviour
    State changing works, background set. Now need to place it at the correct place  and get the game into this working state stuff

---
+++ PandemicShoppingGame/BaseGame.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using PandemicShoppingGame.GameStates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace PandemicShoppingGame
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class BaseGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private State _currentState;
+
+        private State _nextState;
+
+        public void ChangeState(State state)
+        {
+            _nextState = state;
+        }
+
+        public string level;
+
+        private Texture2D _texture;
+        private Vector2 _position;
+
+        public BaseGame(string level)
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.IsFullScreen = true;
+            graphics.HardwareModeSwitch = false;
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            IsMouseVisible = true;
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _texture = Content.Load<Texture2D>("Backgrounds/MainMenuBackground");
+            _position = new Vector2(0, 0);
+
+            _currentState = new MainMenuState(this, graphics.GraphicsDevice, Content);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (_nextState != null)
+            {
+                _currentState = _nextState;
+
+                _nextState = null;
+            }
+
+            _currentState.Update(gameTime);
+
+            _currentState.PostUpdate(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(_texture, _position, Color.White);
+            spriteBatch.End();
+
+            _currentState.Draw(gameTime, spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ PandemicShoppingGame/Component.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+//https://www.youtube.com/watch?v=r5dM0_J7KuY
+
+namespace PandemicShoppingGame
+{
+    public abstract class Component
+    {
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+
+        public abstract void Update(GameTime gameTime);
+    }
+}
